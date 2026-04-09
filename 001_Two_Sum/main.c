d2477650ddf2c6eac4d683a2e3d8387800c3a21a
xm <2053734900@qq.com>
2026-04-09 09:27:41 +0800

    第1次提交

---
+++ 001_Two_Sum/main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int sum(int num1, int num2) {
+    return num1 + num2;
+}
+
+int main() {
+    int num1, num2, result;
+    scanf("%d %d", &num1, &num2);
+    result = sum(num1, num2);
+    printf("%d\n", result);
+    return 0;
+}
